99289bb941560116b1e0c37a508d9f9b9ee45ea6
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-04-28 15:31:31 +0600

    Add files via upload

---
+++ prb_7_!D_Eraser.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == 'B') {
+                ++ans;        
+                i += k - 1;     
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
