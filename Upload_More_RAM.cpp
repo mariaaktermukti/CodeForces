879370aa54e654dc5188065e338ba8097be681fe
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-04 20:59:46 +0600

    Add files via upload

---
+++ Upload_More_RAM.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+const char nl = '\n';
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+void solve() 
+{
+    int n, k;
+    cin >> n >> k;
+    cout << 1 + (n - 1) * k << nl;
+}
+
+int main() 
+{
+	ios::sync_with_stdio(0); cin.tie(0); 
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+}
