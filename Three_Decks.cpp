b6dfa660d792c126db7fcd595c61a532d43322cd
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-31 09:32:54 +0600

    Add files via upload

---
+++ Three_Decks.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if ((a + b + c) % 3 != 0) {
+            cout << "NO" << endl;
+            continue;
+        }
+
+        int x = (a + b + c) / 3;
+
+        if (b <= x) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
