daa2f7b05ea0760b00a23fc05c4cced385e474e7
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-08-21 11:45:43 +0600

    Add files via upload

---
+++ CodeForces 1037 ( div 3 )/No_Casino_in_the_Mountains.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int a[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+        int ans = 0;
+        int cnt = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] == 1)
+            {
+                ans += (cnt + 1) / (k + 1);
+                cnt = 0;
+                continue;
+            }
+            else
+            {
+                cnt++;
+            }
+        }
+        ans += (cnt + 1) / (k + 1);
+        cout << ans << endl;
+    }
+}
