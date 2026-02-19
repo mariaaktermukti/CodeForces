70515e84262d81152f558380a75f0e81d5f4684c
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2026-02-19 20:38:25 +0600

    Add files via upload

--- replace_with_occurrences.cpp
+++ replace_with_occurrences.cpp
@@ -3,56 +3,69 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    int T;
+    if (!(cin >> T))
+        return 0;
+    while (T--)
     {
         int n;
-        cin >> n;
-        vector<int> b(n);
-        for (int i = 0; i < n; i++)
-            cin >> b[i];
+        long long k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i)
+            cin >> a[i];
 
-        vector<vector<int>> pos(n + 1);
-        for (int i = 0; i < n; i++)
-        {
-            pos[b[i]].push_back(i);
-        }
+        sort(a.begin(), a.end());
+        vector<int> l;
+        vector<int> sl;
 
-        bool ok = true;
-        for (int x = 1; x <= n; x++)
+        while (k > 0 && l != a)
         {
-            if (pos[x].size() % x != 0)
+            --k;
+            sl = l;
+            l = a;
+
+            vector<int> F(n + 2, 0);
+            for (int x : a)
             {
-                ok = false;
-                break;
+                if (x >= 0 && x <= n)
+                    F[x]++;
+            }
+
+            int mex = 0;
+            while (mex < (int)F.size() && F[mex] > 0)
+                ++mex;
+
+            vector<int> B;
+            B.reserve(n);
+            int mm = 0;
+            for (int x : a)
+            {
+                if (F[x] > 1)
+                {
+                    B.push_back(mex);
+                }
+                else
+                {
+                    B.push_back(mm);
+                }
+                if (x == mm)
+                    ++mm;
+            }
+
+            sort(B.begin(), B.end());
+            a = move(B);
+
+            if (a == sl)
+            {
+                k &= 1LL;
             }
         }
 
-        if (!ok)
-        {
-            cout << -1 << "\n";
-            continue;
-        }
-
-        vector<int> ans(n, 0);
-        int z = 1;
-
-        for (int x = 1; x <= n; x++)
-        {
-            auto &vec = pos[x];
-            for (int i = 0; i < (int)vec.size(); i++)
-            {
-                ans[vec[i]] = z;
-                if ((i + 1) % x == 0)
-                    z++;
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            cout << ans[i] << (i + 1 == n ? '\n' : ' ');
-        }
+        long long sum = 0;
+        for (int x : a)
+            sum += x;
+        cout << sum << '\n';
     }
     return 0;
 }
